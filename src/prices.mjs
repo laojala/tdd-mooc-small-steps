39e123ae3b6c9989f426d80fdb4cfd71171007f0
Laura Ojala <laojala@gmail.com>
2022-04-15 16:15:35 +0300

    remove redundant function

--- src/prices.mjs
+++ src/prices.mjs
@@ -29,12 +29,6 @@ function createApp(database) {
     }
   }
 
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
-  }
-
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
