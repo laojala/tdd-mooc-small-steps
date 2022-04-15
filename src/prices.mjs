15707bd4922cf8b3c6d164e156c202dd15ab9f71
Laura Ojala <laojala@gmail.com>
2022-04-15 16:28:33 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -86,6 +86,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday_foo = parsePlainDate(row.holiday);
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
