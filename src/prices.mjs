bffe88aa2f3c72f0c351818cc4ab22efcf3161ad
Laura Ojala <laojala@gmail.com>
2022-04-15 15:56:06 +0300

    add temporary function parsePlainDate

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,9 +19,16 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const date2 = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
+
+  function parsePlainDate(dateString) {
+    if (dateString) {
+      return Temporal.PlainDate.from(dateString)
+    }
+  }
 
   function parseDate(dateString) {
     if (dateString) {
