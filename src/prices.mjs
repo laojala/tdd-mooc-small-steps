e21ef1294e040d247c3567e270b9aa5bfecb36bd
Laura Ojala <laojala@gmail.com>
2022-04-15 10:00:04 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -82,6 +82,7 @@ function createApp(database) {
   
   function isHoliday(date) {
     const holidays = database.getHolidays();
+    let foo_date = Temporal.PlainDateTime.from(date.toTemporalInstant().toZonedDateTimeISO("UTC"))
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       let holiday_temporal = Temporal.PlainDateTime.from(row.holiday)
