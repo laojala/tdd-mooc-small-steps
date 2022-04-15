b5a393beb9c7d56d0a5b5db1800d7ca73c224bc9
Laura Ojala <laojala@gmail.com>
2022-04-15 10:39:55 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -84,7 +84,7 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday)
       if (
-        Temporal.PlainDate.compare(date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate(), holiday) == 0
+        Temporal.PlainDate.compare(convert(date), holiday) == 0
       ) {
         return true;
       }
