20498958e1cfe27ef5d4bd556fdf325117412783
Laura Ojala <laojala@gmail.com>
2022-04-15 09:29:38 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,7 +87,7 @@ function createApp(database) {
       if (
         date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate() &&
         //date.toTemporalInstant().toZonedDateTimeISO("UTC").getISOFields().isoYear === holiday.getFullYear() &&
-        date.toTemporalInstant().toZonedDateTimeISO("UTC").getISOFields().isoMonth - 1 === holiday.getMonth() &&
+        //date.toTemporalInstant().toZonedDateTimeISO("UTC").getISOFields().isoMonth - 1 === holiday.getMonth() &&
         date.toTemporalInstant().toZonedDateTimeISO("UTC").getISOFields().isoDay === holiday.getDate() 
       ) {
         return true;
