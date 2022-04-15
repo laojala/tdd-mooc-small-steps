11d7027e090f6797f7853384f4a53ef6dc61df40
Laura Ojala <laojala@gmail.com>
2022-04-15 16:32:35 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -72,7 +72,7 @@ function createApp(database) {
 
   function calculateReduction(date, date_foo) {
     let reduction = 0;
-    if (date_foo && isMonday(date_foo, date_foo) && !isHoliday(date_foo, date_foo)) {
+    if (date_foo && isMonday(date_foo) && !isHoliday(date_foo)) {
       reduction = 35;
     }
     return reduction;
