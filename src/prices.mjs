aa5ef5b4bf25306bed8255e8007e18c38601a78a
Laura Ojala <laojala@gmail.com>
2022-04-15 16:14:39 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -36,11 +36,11 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date2, baseCost) {
+  function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date2, baseCost, date2);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
